d810cb5c906cad7f9f2b1021a9ff1f2dc49dd450
Daniel Kirwan <danielkirwan07@gmail.com>
2021-08-30 14:18:22 +0100

    run and jump animation

--- DungeonEscape/Assets/Scripts/Player.cs
+++ DungeonEscape/Assets/Scripts/Player.cs
@@ -11,10 +11,17 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask _ground;
     private bool _resetJump = false;
     [SerializeField] private Animator _anim;
+    [SerializeField] PlayerAnimation _playerAnimation;
+    private SpriteRenderer _sprite;
+    private bool _jumping = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        if(_sprite == null)
+        {
+            Debug.Log("Sprite is null");
+        }
     }
 
     // Update is called once per frame
@@ -26,14 +33,30 @@ public class Player : MonoBehaviour
     void PlayerMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        _rb.velocity = new Vector2(horizontalInput * _speed, _rb.velocity.y);
-        _anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
-
+        _grounded = Grounded();
+        FlipPlayer(horizontalInput);
         Debug.DrawRay(transform.position, Vector2.down * 0.8f, Color.green);
         if (Input.GetKeyDown(KeyCode.Space) && Grounded())
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
             StartCoroutine(ResetJumpRoutine());
+            _jumping = true;
+            _playerAnimation.JumpPlayer(_jumping);
+        }
+
+        _rb.velocity = new Vector2(horizontalInput * _speed, _rb.velocity.y);
+        _playerAnimation.MovePlayer(horizontalInput);
+    }
+
+    void FlipPlayer(float horizontalInput)
+    {
+        if (horizontalInput > 0f)
+        {
+            _sprite.flipX = false;
+        }
+        else if (horizontalInput < 0f)
+        {
+            _sprite.flipX = true;
         }
     }
 
@@ -45,6 +68,9 @@ public class Player : MonoBehaviour
         {
             if (!_resetJump)
             {
+                Debug.Log("Grounded");
+                _jumping = false;
+                _playerAnimation.JumpPlayer(_jumping);
                 return true;
             }
         }
@@ -55,7 +81,7 @@ public class Player : MonoBehaviour
     IEnumerator ResetJumpRoutine()
     {
         _resetJump = true;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.05f);
         _resetJump = false;
     }
 
---
+++ DungeonEscape/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MovePlayer(float speed)
+    {
+        _anim.SetFloat("Speed", Mathf.Abs(speed));
+    }
+
+    public void JumpPlayer(bool jumping)
+    {
+        _anim.SetBool("Jumping", jumping);
+        Debug.Log("Jumping is " + jumping);
+    }
+
+
+}
